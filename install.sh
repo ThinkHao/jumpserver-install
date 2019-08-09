5a66a3698373e8cf36cbec3d9f55588ec9b71ecb
ThinkHao <jinxinhaowx@gmail.com>
2019-08-09 20:40:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+
+# Check if user is root
+if [ $(id -u) != "0" ]; then
+    echo "Error: You must be root to run this script, please use root to install lnmp"
+    exit 1
+fi
+
 CurPWD=$(cd "$(dirname "$0")"; pwd)
 DOCKER_IP=`echo '172.17.0.0/16'`
 
